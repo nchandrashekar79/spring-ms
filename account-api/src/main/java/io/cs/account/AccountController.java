5e698e202ae2828ddf1313279c062abc96aaf4c0
tss <test@mail.com>
2024-03-17 19:31:38 +0530

    Account ms commit

---
+++ account-api/src/main/java/io/cs/account/AccountController.java
@@ -0,0 +1,15 @@
+package io.cs.account;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("account")
+public class AccountController {
+
+    @GetMapping
+    public String test(){
+        return "Account test";
+    }
+}
