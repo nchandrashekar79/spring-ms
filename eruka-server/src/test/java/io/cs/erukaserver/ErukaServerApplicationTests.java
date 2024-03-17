79e6d2c6ee0d73370a8fab603a2a35ec41f8930a
tss <test@mail.com>
2024-03-17 12:28:53 +0530

    Project init

---
+++ eruka-server/src/test/java/io/cs/erukaserver/ErukaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cs.erukaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErukaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
